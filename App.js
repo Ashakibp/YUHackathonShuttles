524997a2a3d682eefb6d8d9c77fc36ebbf3c201c
Ashakibp <aaronshakib@gmail.com>
2017-10-28 22:55:16 -0400

    ReactBaseline

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View,
+  View, Image, TouchableOpacity, TextInput
 } from 'react-native';
 
 const instructions = Platform.select({
@@ -23,35 +23,74 @@ export default class App extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
+        <Image style={styles.imageStyle } source={require('./Resources/YuLogo.png')} />
         <Text style={styles.welcome}>
-          Welcome to this place
+          Welcome
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit App.js
+          Please Login
         </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <TextInput style = {styles.input}
+                   underlineColorAndroid = "transparent"
+                   placeholder = "Email"
+                   placeholderTextColor = "#000000"
+                   autoCapitalize = "none"
+                   onChangeText = {this.handleEmail}/>
+
+        <TextInput style = {styles.input}
+                   underlineColorAndroid = "transparent"
+                   placeholder = "Password"
+                   placeholderTextColor = "#000000"
+                   autoCapitalize = "none"
+                   onChangeText = {this.handlePassword}/>
+
+        <TouchableOpacity
+            style = {styles.submitButton}
+            onPress = {() => this.login(this.state.email, this.state.password)}>
+          <Text style = {styles.submitButtonText}> Submit </Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#fbfbff',
   },
+  imageStyle: {
+      width: 130,
+      height: 130
+    },
   welcome: {
     fontSize: 30,
     textAlign: 'center',
     margin: 10,
   },
+    input: {
+        margin: 15,
+        height: 40,
+        borderColor: '#000000',
+        borderWidth: 1,
+        width: 300
+    },
+    submitButton: {
+        backgroundColor: '#465edc',
+        padding: 10,
+        margin: 15,
+        height: 40,
+    },
+    submitButtonText:{
+        color: 'white'
+    },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+      fontSize: 20,
+      textAlign: 'center',
+      margin: 10,
   },
+
 });
